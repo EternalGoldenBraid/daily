fd3b10be5c8a1fe8c30ef92fe77f9aaa770099ed
EternalGoldenBraid <gitnicklas@pm.me>
2019-07-21 19:27:29 +0200

    add migration support, config and env var(s)

---
+++ daily/views.py
@@ -0,0 +1,38 @@
+import os
+import datetime
+
+from flask import render_template, request, redirect, session
+from flask_session import Session
+from daily import app
+
+from daily.helpers import login_required
+
+@app.route("/")
+def index():
+    """ Show current data on daily """
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+
+    # Forget any user_id
+    session.clear()
+
+    # Request method was POST
+    if request.method == 'POST':
+
+        # Ensure valid user input
+        if not request.form.get("username"):
+            return ("Invalid username")
+
+        # Query db for existing credentials
+
+           # Log user in
+        else:
+            return redirect("/")
+
+    # Request method was GET
+    else:
+        return render_template("login.html")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='daily',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
