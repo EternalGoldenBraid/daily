5b038bb2d7ceaa487b981aa9ce32b0c3014bb853
EternalGoldenBraid <gitnicklas@protonmail.ch>
2019-08-22 13:01:42 +0300

    First working version of database models.py

--- daily/models.py
+++ daily/models.py
@@ -8,11 +8,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120) ,index=True, unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128), nullable=False)
     
     # Establish Rating objects on User called User.ratings.
     # Establish a .user attribute on Rating which referes to the parent User object
-    ratings = db.relationship('Rating', backref='user')
+    ratings = db.relationship('Rating', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,20 +26,19 @@ class Rating(db.Model):
     rating_sleep = db.Column(db.Integer, index=True, nullable=False)
     meditation = db.Column(db.Integer, nullable=False) # Duration of daily meditation.
     cw = db.Column(db.Integer, nullable=False) # Duration of daily creative work.
-    screen = db.Column(db.Time, nullable=False) # When did user stop bright light exposure.
+    screen = db.Column(db.Time, nullable=False) # When did user stop/reduce bright light exposure.
     rating_day = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
 
     # Establish Event objects on Rating called Rating.events.
     # Establish .rating attribute on Event, which refer to the parent Rating object.
-    events = db.relationship('Event', backref='rating')
+    events = db.relationship('Event', backref='rating', lazy='dynamic')
 
     def __repr__(self):
         return '<Rating of the day: {}, User_id: {}>'.format(self.rating_day, self.user_id)
     
-# Define events table
-
-# Read here http://howto.philippkeller.com/2005/04/24/Tags-Database-schemas/
+# Define events table, by design will have multiple date entries due to multiple unique tags.
+# Can probably be improved
 
 class Event(db.Model):
     __tablename__ = 'event'
@@ -47,19 +46,26 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     duration = db.Column(db.Integer) # In future integrate with Toggl API 
     rating_date = db.Column(db.Date, db.ForeignKey('rating.date'), index=True, nullable=False)
+    event_tag = db.Column(db.String, db.ForeignKey('tag.tag_name'), index=True, nullable=False)
 
-    tags = db.relationship('Tag', backref='event')
-
+    
     def __repr__(self):
-        return '<Tags {}>'.format(self.username)
+        return '<Event on {}>'.format(self.rating_date)
 
 # Define relationships with tags and days
 class Tag(db.Model):
     __tablename__ = 'tag'
 
     id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String, index=True, unique=True, nullable=False)
 
-    
+    # Establish Tag object on Event called Event.tags.
+    # Establish .event attribute on Tag, which refers to the parent Event object.
+    tags = db.relationship('Event', backref='tag', lazy='dynamic')
+
+
+    def __repr__(self):
+        return '<Tag name is {}>'.format(self.tag_name)
 
 
 
