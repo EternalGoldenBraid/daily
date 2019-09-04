bba1d958d80d4df15c9b5f183f38176e47f62abe
EternalGoldenBraid <gitnicklas@pm.ch>
2019-09-04 18:02:34 +0300

    daily add next to login required. testing inconclusive

--- daily/views.py
+++ daily/views.py
@@ -1,7 +1,8 @@
 import os
 import datetime
 
-from flask import render_template, redirect, flash, url_for 
+from flask import render_template, redirect, flash, url_for, request 
+from werkzeug.urls import url_parse
 from daily import app # unnecessary
 from daily.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -28,6 +29,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(url_for(next_page))
         return redirect(url_for('index'))
     return render_template('login.html', title='Log In', form=form)
 
