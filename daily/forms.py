9d4a746af3569932de4fdb5a92ed23dd62a52692
EternalGoldenBraid <gitnicklas@pm.ch>
2019-12-13 16:06:45 +0200

    entry forms

--- daily/forms.py
+++ daily/forms.py
@@ -4,7 +4,12 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username=StringField('Username', validators=[DataRequired()])
+    password=PasswordField('Password', validators=[DataRequired()])
+    remember_me=BooleanField('Remember Me')
+    submit=SubmitField('Sign In')
+
+class EntryForm(FlaskForm):
+    date=DateField('Date', validators=[DateRequired()])
+    sleep_rating=IntegerField('Sleep Rating')
+    meditation=IntegerField('Meditation Duration')
