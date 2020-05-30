16b5d827a848446b82dab477a8c71edafc1f016f
EternalGoldenBraid <gitnicklas@pm.ch>
2020-05-30 21:48:34 +0300

    hold op on multi-user support, just for me for now

--- daily/views.py
+++ daily/views.py
@@ -275,34 +275,46 @@ def login():
 #The inverse of this function is url_unparse().
 
 @app.route("/register", methods=["GET", "POST"])
-def login():
+def register():
     """
     Registers user 
     """
 
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegisterForm()
-    if form.validate_on_submit():         
-    # Check if request was a POST request 
-        # Attempt to fetch users username from the database, 
-        # take the first result
-        user = User.query.filter_by(username=form.username.data).first()
+    # Awaiting rating table modification for multi user support
+    return url_for('register')
 
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        
-        # Forward to the page the attempted to get at before authentication
-        if not next_page or url_parse(next_page) != '':
-            return redirect(url_for('index'))
-
-        return redirect(next_page)
-
-    return render_template('login.html', title='Log In', form=form)
+#    if form.password != form.password_confimation:
+#        flash("Passwords don't match")
+#        return redirect(url_for('register'))
+#
+#    if current_user.is_authenticated:
+#        return redirect(url_for('index'))
+#    form = RegisterForm()
+#    if form.validate_on_submit():         
+#    # Check if request was a POST request 
+#
+#        # Check if user already exists
+#        username = form.username.data
+#        user = User.query.filter_by(username=username).first()
+#
+#        if user is not None:
+#            flash('Username already exists')
+#            return redirect(url_for('register'))
+#
+#        # Create user
+#        u = User(username=username, email=#TODO)
+#        u.set_password(form.password.data)
+#
+#        login_user(user, remember=form.remember_me.data)
+#        next_page = request.args.get('next')
+#        
+#        # Forward to the page the attempted to get at before authentication
+#        if not next_page or url_parse(next_page) != '':
+#            return redirect(url_for('index'))
+#
+#        return redirect(next_page)
+#
+#    return render_template('login.html', title='Log In', form=form)
 
 # url_parse() Parses a URL from a string into a URL tuple. 
 #If the URL is lacking a scheme it can be provided as second argument. 
