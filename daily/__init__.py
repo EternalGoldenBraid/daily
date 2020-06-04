4899bf1c9aeaf11e05b3fb35a8af84e9f04c46e1
EternalGoldenBraid <gitnicklas@pm.ch>
2020-06-04 09:54:27 +0300

    Changed date unique constraint and some strlength constraints

--- daily/__init__.py
+++ daily/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, compare_type=True)
 
 
 #View module (view functions) must be imported after the application object is created.
