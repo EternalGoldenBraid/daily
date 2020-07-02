dc68341f6fb3f3b5fd25f66d5daf6eb9276aa312
EternalGoldenBraid <gitnicklas@pm.ch>
2020-07-02 20:50:40 +0300

    added / alongside index as a route

--- daily/views.py
+++ daily/views.py
@@ -16,6 +16,7 @@ from flask_login import (current_user, login_user,
 from werkzeug.urls import url_parse
 
 
+@app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
