deec176d2836aa56c2f60121c7f8264c35e8e16d
EternalGoldenBraid <gitnicklas@pm.ch>
2020-07-19 20:26:00 +0300

    revert commit

--- daily_test.py
+++
@@ -1,7 +0,0 @@
-from daily import app, db
-from daily.models import User, Rating, Event, Tag
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Rating': Rating, 'Event': Event,
-            'Tag': Tag}
