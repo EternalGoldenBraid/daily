7c3f7a56133876dcddf71aac0fb1c0292522aefa
Nicklas <nicklas@localhost.localdomain>
2021-11-27 23:08:17 +0200

    Played around with PCA and svd. Need to figure out a functional way to form the covariance matrix

--- daily/data_analysis/views.py
+++ daily/data_analysis/views.py
@@ -11,6 +11,7 @@ import matplotlib
 import matplotlib.pyplot as plt,mpld3
 import matplotlib.dates as mdates
 
+from collections import Counter
 import numpy as np
 import pandas as pd
 from numpy.random import default_rng
@@ -25,8 +26,9 @@ import os
 def data():
 
     rng = default_rng()
-    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots(2, figsize=(8,8), projection='3d')
 
+    """ Sleep ratings """
     engine = db.get_engine()
     s= pd.read_sql('rating',engine, index_col=False)
 
@@ -43,17 +45,78 @@ def data():
     #s_date = s_date.reindex(idx)
 
     #s_date = s_date.set_index(s_date['date'])
-    print(s_date[:3])
-    ax.plot(s_date['rating_sleep'])
-    ax.set_xticks(np.arange(s_date['date'].size))
-    ax.set_xticklabels(s_date['date'])
-    plt.xticks(rotation=30)
+    #s_date = s_date[:3]
+    #idx = pd.date_range(min(s_date['date']), max(s_date['date']), freq='D')
+
+
+    #fig.autofmt_xdate()
+    #ax[0].scatter(range(209),s_date['rating_sleep'],s=5)
+    #ax.plot(s_date['rating_sleep'],c='g')
+    #ax.plot('date',data=s_date['rating_sleep'])
+    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%y'))
+    #ax.xaxis.set_major_locator(mdates.MonthLocator())
+    #ax.xaxis.set_minor_locator(mdates.MonthLocator())
+    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%b'))
+    #ax.set_xticks(np.arange(s_date['date'].size))
+    #ax.set_xticklabels(s_date['date'])
+    #ax.xaxis.set_major_locator(mdates.YearLocator(1, month=1, day=1))
+    #plt.xticks(rotation=30)
+
+
+
+    """ TAGS """
+    tags = pd.read_sql('tag',engine, index_col=False)
+    tags.columns = tags.columns.str.replace('id','tag.id')
+
+    event_tag = pd.read_sql('event_tags',engine, index_col=False)
+
+    events = pd.read_sql('event',engine, index_col=False)
+    events.columns = events.columns.str.replace('id','event.id')
+
+    match = pd.merge(event_tag,events,on='event.id')
+    match = pd.merge(match,tags,on='tag.id')
+
+    tags_name = match.groupby('rating_date')['tag_name'].apply(list)
+    tags_id = match.groupby('rating_date')['tag.id'].agg(list) # Series
+
+    data = np.zeros((events.size,tags.size))
+    for row, day in enumerate(tags_id):
+        a = Counter(day)
+        for col in a.keys():
+            data[row][col]=a[col]
+
+
+    
+    print(data)
+    #data = (data-np.mean(data,axis=0))/np.std(data,axis=0)
+    data = (data-np.mean(data,axis=0))
+    cov = np.cov(data, rowvar=False)
+    e_values, e_vectors = np.linalg.eigh(cov)
+    print(data)
+
+    # Sort eigenvalues and their eigenvectors in descending order
+    e_ind_order = np.flip(e_values.argsort())
+    e_values = e_values[e_ind_order]
+    # note that we have to re-order the columns, not rows
+    e_vectors = e_vectors[:, e_ind_order] 
+    # pca
+    prin_comp_evd = data @ e_vectors
+
+    print(prin_comp_evd)
+
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection='3d')
+    #ax[1].scatter(
+    ax.scatter(
+        prin_comp_evd[:, 0], prin_comp_evd[:, 1], prin_comp_evd[:, 2]
+        )
+    plt.show()
 
     #plt.gcf().autofmt_xdate()
 
     #print (mpld3.fig_to_html(fig1, d3_url=None, mpld3_url=None, no_extras=False, template_type='general', figid=None, use_http=False))
-    html = mpld3.fig_to_html(fig)
-    file = open("daily/templates/data_analysis/data.html","w")
-    file.write(html)
-    file.close()
+    #html = mpld3.fig_to_html(fig)
+    #file = open("daily/templates/data_analysis/data.html","w")
+    #file.write(html)
+    #file.close()
     return render_template("data_analysis/data.html")
